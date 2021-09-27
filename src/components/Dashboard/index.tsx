799d816138edf3578761098c1863836361bba46c
iury.franca <iurygfranca@gmail.com>
2021-09-27 16:28:28 -0300

    feat: adding initial config api conection ✨

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "./styles";
 import { Summary } from "../Summary";
+import { TransactionTable } from "../TransactionsTables";
 
 export function Dashboard(){
   return (
     <Container>
-      <Summary/>
+      <Summary />
+      <TransactionTable/>
   </Container>
   );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,6 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    --background: #f0f2f5;
     --green: #33cc95;
     --red: #E52E4D;
     --blue: #5429CC;
