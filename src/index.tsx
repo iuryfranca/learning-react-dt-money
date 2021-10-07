e6ed8ccf51614b1b1c54fdeaa6e63c9433e5a0fd
iury.franca <iurygfranca@gmail.com>
2021-10-07 13:40:13 -0300

    feat: using API to render data ✨

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transaction: [
+        {
+          id: 1,
+          title: "Freelancer de website",
+          type: 'deposit',
+          category: 'Dev',
+          amount: '6000',
+          createAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 1,
+          title: "Aluguel",
+          type: 'withdraw',
+          category: 'Casa',
+          amount: '1100',
+          createAt: new Date('2021-02-14 11:00:00')
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api'
 
     this.get('transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'transition 1',
-          amount: 1000,
-          type: 'deposit',
-          category: 'Food',
-          createDate: new Date(),
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transitions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transition', data);
     })
   }
 })
