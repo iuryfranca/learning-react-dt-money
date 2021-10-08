b7480ac12348f1e8cdbd99ecac3b151b4085659c
iury.franca <iurygfranca@gmail.com>
2021-10-08 17:44:28 -0300

    Revert "feat: using API to render data ✨"
    This reverts commit e6ed8ccf51614b1b1c54fdeaa6e63c9433e5a0fd.

--- src/components/TransactionsTables/index.tsx
+++ src/components/TransactionsTables/index.tsx
@@ -1,22 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
-interface Transaction {
-  id: number;
-  title: string;
-  amount: number;
-  type: string;
-  category: string;
-  createAt: string;
-}
-
 export function TransactionTable() {
-  const [transactions, setTransactions] = useState<Transaction[]>([])
-
   useEffect(() => {
     api.get('transactions')
-      .then(response => setTransactions(response.data.transactions))
+      .then(response => console.log(response.data))
   }, []);
 
   return (
@@ -32,23 +21,18 @@ export function TransactionTable() {
         </thead>
 
         <tbody>
-            {transactions.map(transaction => (
-              <tr key={transaction.id}>
-                <td>{transaction.title}</td>
-                <td className={transaction.type}>
-                  {new Intl.NumberFormat('pt-BR', {
-                      style: 'currency',
-                      currency: 'BR'
-                  }).format(transaction.amount)}
-                </td>
-                <td>{transaction.category}</td>
-                <td>
-                  {new Intl.DateTimeFormat().format(
-                    new Date(transaction.amount)
-                  )}
-                </td>
-              </tr>
-            ))}
+          <tr>
+            <td>Desenvolvimento de Website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">R$-1.100</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
         </tbody>
       </table>
     </Container>
