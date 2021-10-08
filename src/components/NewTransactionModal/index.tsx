9d9c51e9e5899cadfbd0ded24284a83eb5d9c43b
iury.franca <iurygfranca@gmail.com>
2021-10-08 17:55:08 -0300

    Revert "Revert "feat: using data sharing with context ✨""
    This reverts commit c9737084070b0fd5472ba5448ed5cc8d0bb6052a.

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,5 @@
+import { FormEvent, useState, useContext } from 'react';
 import Modal from 'react-modal'
-
-import { FormEvent, useState } from 'react';
 import { Container } from './styles'
 import { TransactionTypeContainer, RadioBox } from './styles';
 
@@ -9,6 +8,8 @@ import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import { api } from '../../services/api';
 
+import { TransactionsContext } from '../../TransactionsContext';
+
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -16,22 +17,22 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const { createTransaction } = useContext(TransactionsContext);
+
   const [title, setTitle] = useState('');
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit')
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault()
 
-    const data = {
+    createTransaction({
       title,
-      value,
+      amount,
       category,
-      type
-    }
-
-    api.post('/transactions', data)
+      type,
+    })
   }
 
   return (
@@ -61,8 +62,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <input
           type="number"
           placeholder="valor"
-          value={value}
-          onChange={event => setValue(Number(event.target.value))}
+          value={amount}
+          onChange={event => setAmount(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
